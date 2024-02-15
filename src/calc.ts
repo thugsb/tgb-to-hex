4bf33cd86feba43eb750576baf5a20303aae1234
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-02-15 13:00:31 +0000

    Convert RGB to Hex function

--- src/calc.ts
+++ src/calc.ts
@@ -1,3 +1,15 @@
-export function add(x: number, y: number): number {
-  return x + y;
+export function rgbToHex(colors: number[]): string {
+  const boundedNumbers = colors.map((color: number): number => {
+    if (color < 0) {
+      return 0;
+    } else if (color > 255) {
+      return 255;
+    }
+    return color;
+  });
+  const convertedNumbers = boundedNumbers.map((col: number) => {
+    return col.toString(16).toUpperCase();
+  });
+
+  return convertedNumbers.join("");
 }
